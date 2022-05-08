d98e6293f55d8a358accf4a8a532914dd838086f
renxzen <renxzen@gmail.com>
2022-05-07 19:40:40 -0500

    commented outputChannel

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,62 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+var savingState = false;
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "multiformatter" is now active!');
+	const extConfig = vscode.workspace.getConfiguration("multiformatter");
+	const config = vscode.workspace.getConfiguration("", {
+		languageId: extConfig.get("language")!,
+	});
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('multiformatter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Multiformatter!');
+	// const outputChannel = vscode.window.createOutputChannel("multiformatter");
+	// outputChannel.appendLine("Multiformatter active.");
+	// outputChannel.appendLine("Formatter 1: " + extConfig.get("formatter1"));
+	// outputChannel.appendLine("Formatter 2: " + extConfig.get("formatter2"));
+
+	let disposable = vscode.commands.registerCommand(
+		"multiformatter.multiformat",
+		() => {}
+	);
+
+	vscode.workspace.onDidSaveTextDocument((doc) => {
+		if (
+			!savingState &&
+			doc.languageId === extConfig.get("language") &&
+			extConfig.get("enabled")
+		) {
+			savingState = true;
+
+			config
+				.update("editor.defaultFormatter", extConfig.get("formatter1"))
+				.then(() => {
+					vscode.commands
+						.executeCommand("editor.action.formatDocument")
+						.then(() => {
+							config
+								.update(
+									"editor.defaultFormatter",
+									extConfig.get("formatter2")
+								)
+								.then(() => {
+									vscode.commands
+										.executeCommand(
+											"editor.action.formatDocument"
+										)
+										.then(() => {
+											doc.save().then(() => {
+												savingState = false;
+												// outputChannel.appendLine(
+												// 	"File multiformattted."
+												// );
+											});
+										});
+								});
+						});
+				});
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
