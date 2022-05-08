c70a87aafa1c0d61a75c090a00dba48b211d61af
renxzen <renxzen@gmail.com>
2022-05-07 20:33:33 -0500

    update

--- src/extension.ts
+++ src/extension.ts
@@ -8,11 +8,6 @@ export function activate(context: vscode.ExtensionContext) {
 		languageId: extConfig.get("language")!,
 	});
 
-	// const outputChannel = vscode.window.createOutputChannel("multiformatter");
-	// outputChannel.appendLine("Multiformatter active.");
-	// outputChannel.appendLine("Formatter 1: " + extConfig.get("formatter1"));
-	// outputChannel.appendLine("Formatter 2: " + extConfig.get("formatter2"));
-
 	let disposable = vscode.commands.registerCommand(
 		"multiformatter.multiformat",
 		() => {
